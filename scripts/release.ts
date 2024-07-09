07f7303e87bb14d87b71a9af24faa32e64c66bd5
wangxingkang <wang_xingkang@qq.com>
2024-07-09 11:04:35 +0800

    chore: add release scripts

---
+++ scripts/release.ts
@@ -0,0 +1,23 @@
+import 'zx/globals'
+import simpleGit from 'simple-git';
+import { createLogger } from './utils/signale';
+
+const git = simpleGit();
+const logger = createLogger('release');
+
+(async () => {
+  const status = await git.status();
+  const branch = await git.branch();
+
+  logger.info(`当前分支: ${branch.current}`);
+
+  if (status.files.length !== 0) {
+    logger.error('还有未提交的代码');
+    process.exit(1);
+  }
+
+  const pull = await git.pull();
+  if (pull?.summary?.changes) {
+    logger.info('远程存在未拉取的提交');
+  }
+})();
