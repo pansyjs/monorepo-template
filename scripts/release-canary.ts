2870079c769c5daae9e929b640d7bd39fa785ed6
wangxingkang <wang_xingkang@qq.com>
2024-07-09 11:29:44 +0800

    chore: 完善发布脚本

--- scripts/release-canary.ts
+++ scripts/release-canary.ts
@@ -1,37 +1,33 @@
 import 'zx/globals'
-import path from 'node:path';
-import fs from 'fs-extra';
 import simpleGit from 'simple-git';
-import dayjs from 'dayjs';
 import { createLogger } from './utils/signale';
-import { generateRandomString } from './utils/string';
 import { MASTER_BRANCH } from './.internal/constants';
 
 const git = simpleGit();
-const cwd = process.cwd();
-const logger = createLogger('publish:beta');
-
-function getNextVersion(branch: string) {
-  const branchName = branch.replace(/\//g, '-');
-  const random = generateRandomString();
-
-  return `${branchName}-${random}-${dayjs().format('YYYYMMDD')}`;
-}
+const logger = createLogger('release:canary');
 
 (async () => {
-  const packageJsonPath = path.join(cwd, 'package.json');
   const branch = await git.branch();
-  const current = await fs.readJSON(packageJsonPath);
+
+  logger.info(`当前分支: ${branch.current}`);
 
   if (branch.current === MASTER_BRANCH) {
     logger.error('不能在主分支发布');
     process.exit(1);
   }
 
-  const version = getNextVersion(branch.current)
-  current.version = version;
+  logger.info(`当前分支: ${branch.current}`);
 
-  logger.info(`修改 ${current.name} 版本为 ${version}`);
+  const pull = await git.pull();
+  if (pull?.summary?.changes) {
+    logger.info('远程存在未拉取的提交');
+  }
 
-  await fs.writeJSON(packageJsonPath, current, { spaces: 2 });
+  logger.info(`修改变更包的版本`);
+
+  await $`nx run-many --target=canary:version`;
+
+  logger.info(`发布所有变更包`);
+
+  await $`nx run-many --target=publish --tag canary`;
 })();
